c033a03461cc125ddf1bc12bc66c1ca67bb3283b
AntonKrinichnyi <a.krinichnyi@gmail.com>
2025-02-09 19:35:55 +0200

    Add another station models

--- station/models.py
+++ station/models.py
@@ -32,3 +32,49 @@ class Crew(models.Model):
     
     def __str__(self):
         return self.full_name()
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    
+    def __str__(self):
+        return f"{self.name} station"
+
+
+class Route(models.Model):
+    source = models.ForeignKey(Station, on_delete=models.CASCADE)
+    destination = models.ForeignKey(Station, on_delete=models.CASCADE)
+    distance = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.source} - {self.destination}"
+
+
+class Journey(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    train = models.ForeignKey(Train, on_delete=models.CASCADE)
+    crew = models.ManyToManyField(Crew)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    
+    def __str__(self):
+        return f"{self.route}: {self.departure_time}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    
+
+class Ticket(models.Model):
+    cargo = models.IntegerField()
+    seat = models.IntegerField()
+    journey = models.ForeignKey(Journey, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.journey} cargo: {self.cargo}, seat: {self.seat}"
